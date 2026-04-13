cbde3d38fa6449a608dd924ee0d4e8fd69f6f959
Chris Khoo <chris.khoo@gmail.com>
2026-04-12 22:59:15 -0700

    hello-world-quantum

---
+++ hello-world-quantum.py
@@ -0,0 +1,17 @@
+from braket.circuits import Circuit
+from braket.devices import LocalSimulator
+from braket.aws import AwsDevice
+
+
+# Create a simple circuit
+circuit = Circuit().h(0).measure(0)
+
+# Use local simulator (no cost)
+# device = LocalSimulator()
+device = AwsDevice("arn:aws:braket:us-west-1::device/qpu/rigetti/Ankaa-3")
+
+# Run the circuit
+result = device.run(circuit, shots=100).result()
+
+# Print results
+print(result.measurement_counts)
